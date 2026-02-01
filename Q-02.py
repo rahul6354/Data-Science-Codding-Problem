e28c98070e76e142d46454ecd4665ced69dbfedf
Rahul Garwal <rahulgarawal2@gmail.com>
2026-02-01 12:42:30 +0530

    new commit

---
+++ Q-02.py
@@ -0,0 +1,7 @@
+# cheack max number of two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+if num1 > num2:
+    print(f"{num1} is maximum")
+else:
+    print(f"{num2} is maximum")
