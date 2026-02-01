fd1b40e7d60de76b1efe5ec1cac8bd6948611f72
Rahul Garwal <rahulgarawal2@gmail.com>
2026-02-01 12:22:38 +0530

    Add Q-01.py

---
+++ Q-01.py
@@ -0,0 +1,9 @@
+# cheack even or odd
+# 2 % 2 == 0  # even
+# 3 % 2 != 0  # odd
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f"{num} is even")
+else:
+    print(f"{num} is odd")    
