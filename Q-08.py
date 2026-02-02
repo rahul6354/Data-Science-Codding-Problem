904f72e81f79aaff15c219d1efaf24bf6c2091ff
Rahul Garwal <rahulgarawal2@gmail.com>
2026-02-02 14:38:27 +0530

    count digit in a number.

--- Q-08.py
+++ Q-08.py
@@ -1,5 +1,11 @@
 
 # Count digit in a number.
+# Example:  jab bhi number ko 10 se divide karenge to last digit remove ho jayega. 
+#            or last digit ko count kar sakte hai.
+# 567 // 10 = 56
+# 56  // 10 = 5
+# 5   // 10 = 0
+
 num = int(input("Enter a number: "))
 count = 0
 while num != 0:
